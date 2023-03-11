552bd5ab9e03194ce84bb0a5ce5fd7ccbf9fda6c
luximus-hunter <thomaslipman10@gmail.com>
2023-03-11 14:06:28 +0100

    Dark mode 🚀

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,39 @@
 <script lang="ts">
   import "../app.postcss";
-  import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, Button } from "flowbite-svelte";
-  // import Icon from "../components/Icon.svelte";
-  // import { browser } from "$app/environment";
+  import { browser } from "$app/environment";
 
-  // let app: HTMLElement;
-  // let darkMode: boolean;
-  //
-  // if (browser) {
-  //   app = document.getElementById("app");
-  //   darkMode = localStorage.theme === "dark";
-  // }
-  //
-  // const darkModeToggle = () => {
-  //   darkMode = !darkMode;
-  //
-  //   if (darkMode) {
-  //     app.classList.add("dark");
-  //     localStorage.theme = "dark";
-  //   } else {
-  //     app.classList.remove("dark");
-  //     localStorage.theme = "light";
-  //   }
-  // };
+  import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, Button } from "flowbite-svelte";
+  import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
+  let app: HTMLElement;
+  let darkMode: boolean;
+
+  if (browser) {
+    app = document.getElementById("app");
+    darkMode = localStorage.theme === "dark";
+
+    if (darkMode) {
+      app.classList.add("dark");
+    } else {
+      app.classList.remove("dark");
+    }
+  }
+
+
+  const darkModeToggle = () => {
+    darkMode = !darkMode;
+
+    if (darkMode) {
+      app.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      app.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  };
 </script>
 
-<!--<div class={darkMode ? "dark" : ""}>-->
 <div class="flex flex-col min-h-screen dark:bg-gray-900">
   <div class="flex-1">
     <slot />
@@ -37,10 +45,13 @@
       ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
     >
       <FooterLink href="/about">About</FooterLink>
-      <!--<Button class="!p-3" on:click={darkModeToggle}>-->
-      <!--<Icon color="white" size={16} name={darkMode ? "sun" : "moon"} />-->
-      <!--</Button>-->
+      <Button class="!p-3" on:click={darkModeToggle}>
+        {#if (darkMode)}
+          <Fa icon={faSun}></Fa>
+        {:else }
+          <Fa icon={faMoon}></Fa>
+        {/if}
+      </Button>
     </FooterLinkGroup>
   </Footer>
 </div>
-<!--</div>-->
