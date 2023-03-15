8ab2a435f33dbb6cd9bee81149e6324d6742328f
luximus-hunter <thomaslipman10@gmail.com>
2023-03-15 20:48:35 +0100

    About update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,180 +1,140 @@
 <script lang="ts">
-	import {
-		Card,
-		Button,
-		Heading,
-		Breadcrumb,
-		BreadcrumbItem,
-		P,
-		Span,
-		List,
-		Li
-	} from 'flowbite-svelte';
-	import Link from '../../components/Link.svelte';
+  import {
+    Card,
+    Button,
+    Heading,
+    Breadcrumb,
+    BreadcrumbItem,
+    P,
+    Span,
+    List,
+    Li
+  } from "flowbite-svelte";
+  import Link from "../../components/Link.svelte";
 </script>
 
 <svelte:head>
-	<title>EEN - About</title>
+  <title>EEN - About</title>
 </svelte:head>
 
 <Breadcrumb
-	aria-label="Menu"
-	class="p-4 bg-white md:flex md:p-6 dark:bg-gray-800 z-20 w-full rounded-none border-none"
-	solid
+  aria-label="Menu"
+  class="p-4 bg-white md:flex md:p-6 dark:bg-gray-800 z-20 w-full rounded-none border-none"
+  solid
 >
-	<BreadcrumbItem href="/" home>EEN</BreadcrumbItem>
-	<BreadcrumbItem href="/about">About</BreadcrumbItem>
+  <BreadcrumbItem href="/" home>EEN</BreadcrumbItem>
+  <BreadcrumbItem href="/about">About</BreadcrumbItem>
 </Breadcrumb>
 
 <div class="grid md:grid-cols-3 grid-cols-1 gap-3 m-3">
-	<Card size="none">
-		<Heading tag="h2" class="text-center mb-2">About EEN&trade;</Heading>
-		<P justify
-			>EEN&trade; was made by Thomas Lipman as an assignment for Windesheim University of
-			Applied Sciences. EEN&trade; is a straight-up ripoff of UNO&trade;, no need to hide that
-			fact.</P
-		>
-		<Button class="mt-3" outline href="https://github.com/luximus-hunter/een-frontend">
-			Client Source
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-5 h-5 ml-2"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-				/>
-			</svg>
-		</Button>
-		<Button class="mt-3" outline href="https://github.com/luximus-hunter/een-backend">
-			Server Source
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-5 h-5 ml-2"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-				/>
-			</svg>
-		</Button>
-		<Button class="mt-3" outline href="/">Back</Button>
-	</Card>
-	<Card size="none">
-		<Heading tag="h2" class="text-center mb-2">About the Dev</Heading>
-		<P justify
-			>I'm a junior software developer with knowledge in web- and desktop development studying
-			ICT at Windesheim University of Applied Sciences. Read more about me on my website.</P
-		>
-		<Button class="mt-3" disabled color="yellow" outline>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">About EEN&trade;</Heading>
+    <P justify
+    >EEN&trade; was made by Thomas Lipman as an assignment for Windesheim University of
+      Applied Sciences. EEN&trade; is a straight-up ripoff of UNO&trade;, no need to hide that
+      fact.</P
+    >
+    <Button class="mt-3" outline href="/">Back</Button>
+  </Card>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">About the Dev</Heading>
+    <P justify
+    >I'm a junior software developer with knowledge in web- and desktop development studying
+      ICT at Windesheim University of Applied Sciences. Read more about me on my website.</P
+    >
+    <Button class="mt-3" disabled color="yellow" outline>
 			<Span class="line-through mr-3">
 				<span class="inline-flex items-center">
 					Website
 					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-5 h-5 ml-2"
-					>
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5 ml-2"
+          >
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-						/>
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            />
 					</svg>
 				</span>
 			</Span>
-			Under Construction
-		</Button>
-	</Card>
-	<Card size="none">
-		<Heading tag="h2" class="text-center mb-2">Technologies used</Heading>
-		<div class="flex space-x-3">
-			<div class="flex-1">
-				<Heading tag="h4" class="mb-2">Client</Heading>
-				<List tag="ul" class="space-y-1">
-					<Li>
-						<Link href="https://kit.svelte.dev">SvelteKit</Link>
-					</Li>
-					<List tag="ol" class="pl-5 mt-2 space-y-1">
-						<Li>
-							<Link href="https://www.typescriptlang.org/">TypeScript</Link>
-						</Li>
-						<Li>
-							<Link href="https://sass-lang.com/">SCSS</Link>
-						</Li>
-					</List>
-					<Li>
-						<Link href="https://tailwindcss.com/">TailWind</Link>
-					</Li>
-					<Li>
-						<Link href="https://flowbite-svelte.com">Flowbite</Link>
-					</Li>
-					<Li>
-						<Link href="https://learn.microsoft.com/en-us/aspnet/core/signalr"
-							>SignalR</Link
-						>
-					</Li>
-					<Li>
-						<Link href="https://www.dicebear.com/">DiceBear</Link>
-					</Li>
-					<Li>
-						<Link href="https://fontawesome.com/">FontAwesome</Link>
-					</Li>
-					<List tag="ol" class="pl-5 mt-2 space-y-1">
-						<Li>
-							<Link href="https://cweili.github.io/svelte-fa/">Svelte-Fa</Link>
-						</Li>
-					</List>
-				</List>
-			</div>
-			<div class="flex-1">
-				<Heading tag="h4" class="mb-2">Server</Heading>
-				<List tag="ul" class="space-y-1">
-					<Li>
-						<Link href="https://dotnet.microsoft.com/en-us/">.NET Core</Link>
-					</Li>
-					<List tag="ol" class="pl-5 mt-2 space-y-1">
-						<Li>
-							<Link href="https://learn.microsoft.com/en-us/dotnet/csharp/">C#</Link>
-						</Li>
-					</List>
-					<Li>
-						<Link href="https://bcrypt.online/">BCrypt</Link>
-					</Li>
-					<List tag="ol" class="pl-5 mt-2 space-y-1">
-						<Li>
-							<Link href="https://www.nuget.org/packages/BCrypt.Net-Next">NuGet</Link>
-						</Li>
-					</List>
-					<Li>
-						<Link href="https://learn.microsoft.com/en-us/aspnet/core/signalr"
-							>SignalR</Link
-						>
-					</Li>
-					<Li>
-						<Link href="https://swagger.io/tools/swagger-ui/">SwaggerUI</Link>
-					</Li>
-					<Li>
-						<Link href="https://www.newtonsoft.com/">Newtonsoft</Link>
-					</Li>
-					<Li>
-						<Link href="https://www.dicebear.com/">DiceBear</Link>
-					</Li>
-				</List>
-			</div>
-		</div>
-	</Card>
+      Under Construction
+    </Button>
+  </Card>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">Source Code</Heading>
+    <Button class="mt-3" outline href="https://github.com/luximus-hunter/een-frontend">
+      Client Source
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5 ml-2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+        />
+      </svg>
+    </Button>
+    <Button class="mt-3" outline href="https://github.com/luximus-hunter/een-backend">
+      Server Source
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5 ml-2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+        />
+      </svg>
+    </Button>
+  </Card>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">Client Technologies</Heading>
+    <div class="grid grid-cols-5 gap-3">
+      <img src="icons/svelte.svg" alt="svelte" />
+      <img src="icons/typescript.svg" alt="typescript" />
+      <img src="icons/sass.svg" alt="sass" />
+      <img src="icons/tailwindcss.svg" alt="tailwind" />
+      <img src="icons/flowbite.svg" alt="svelte" />
+      <img src="icons/prettier.svg" alt="svelte" />
+      <img src="icons/eslint.svg" alt="svelte" />
+      <img src="icons/signalr.svg" alt="svelte" />
+      <img src="icons/fontawesome.svg" alt="svelte" />
+      <img src="icons/dicebear.svg" alt="svelte" />
+    </div>
+  </Card>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">Server Technologies</Heading>
+    <div class="grid grid-cols-5 gap-3">
+      <img src="icons/dotnet.svg" alt="svelte" />
+      <img src="icons/csharp.svg" alt="svelte" />
+      <img src="icons/signalr.svg" alt="svelte" />
+      <img src="icons/swagger.svg" alt="svelte" />
+      <img src="icons/bcrypt.svg" alt="svelte" />
+    </div>
+  </Card>
+  <Card size="none">
+    <Heading tag="h2" class="text-center mb-2">Tools used</Heading>
+    <div class="grid grid-cols-5 gap-3">
+      <img src="icons/rider.svg" alt="svelte" />
+      <img src="icons/webstorm.svg" alt="svelte" />
+      <img src="icons/windowsterminal.svg" alt="svelte" />
+      <img src="icons/github.svg" alt="svelte" />
+      <img src="icons/docker.svg" alt="svelte" />
+    </div>
+  </Card>
 </div>
--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,68 +1,68 @@
 <script lang="ts">
-	import {
-		Card,
-		Button,
-		Heading,
-		Breadcrumb,
-		BreadcrumbItem,
-		Checkbox,
-		Listgroup
-	} from 'flowbite-svelte';
+  import {
+    Card,
+    Button,
+    Heading,
+    Breadcrumb,
+    BreadcrumbItem,
+    Checkbox,
+    Listgroup
+  } from "flowbite-svelte";
 
-	const todos: { done: boolean; task: string }[] = [
-		{ done: true, task: 'In-game error message not showing' },
-		{ done: true, task: 'Player dock to full width' },
-		{ done: true, task: 'Hide opponent cards above certain number of cards' },
-		{ done: true, task: 'Dont look through opponents cards' },
-		{ done: true, task: 'Spinner when players are loading in' },
-		{ done: true, task: 'Form spinners on request' },
-		{ done: true, task: 'Put on GitHub' },
-		{ done: true, task: 'FontAwesome icons' },
-		{ done: true, task: 'Card scale based on width' },
-		{ done: true, task: 'Fix wild card logic' },
-		{ done: true, task: 'Fix win logic' },
-		{ done: true, task: 'Somewhat mobile responsive' },
-		{ done: true, task: 'Dark mode' },
-		{ done: true, task: 'Favicon' },
-		{ done: true, task: 'Dockerize client' },
-		{ done: true, task: 'Dockerize server' },
-		{ done: true, task: 'Put on Windesheim servers' },
-		{ done: true, task: 'Un-hardcode api url' },
-		{ done: true, task: 'Page titles' },
-		{ done: true, task: 'Rules page' },
-		{ done: true, task: 'WebSocket player validation with Dictionary & connection id' },
-		{ done: true, task: 'Remove game from server when not in use' },
-		{ done: false, task: 'TO & FO' },
-		{ done: false, task: 'Fix dark mode colors' },
-		{ done: false, task: 'Fix docker for backend' },
-		{ done: false, task: 'Code refactor' },
-		{ done: false, task: 'Svelte warnings for unset properties' },
-		{ done: false, task: 'Tauri app' },
-		{ done: false, task: 'Play test!' }
-	];
+  const todos: { done: boolean; task: string }[] = [
+    { done: true, task: "In-game error message not showing" },
+    { done: true, task: "Player dock to full width" },
+    { done: true, task: "Hide opponent cards above certain number of cards" },
+    { done: true, task: "Dont look through opponents cards" },
+    { done: true, task: "Spinner when players are loading in" },
+    { done: true, task: "Form spinners on request" },
+    { done: true, task: "Put on GitHub" },
+    { done: true, task: "FontAwesome icons" },
+    { done: true, task: "Card scale based on width" },
+    { done: true, task: "Fix wild card logic" },
+    { done: true, task: "Fix win logic" },
+    { done: true, task: "Somewhat mobile responsive" },
+    { done: true, task: "Dark mode" },
+    { done: true, task: "Favicon" },
+    { done: true, task: "Dockerize client" },
+    { done: true, task: "Dockerize server" },
+    { done: true, task: "Put on Windesheim servers" },
+    { done: true, task: "Un-hardcode api url" },
+    { done: true, task: "Page titles" },
+    { done: true, task: "Rules page" },
+    { done: true, task: "WebSocket player validation with Dictionary & connection id" },
+    { done: true, task: "Remove game from server when not in use" },
+    { done: true, task: "Fix dark mode colors" },
+    { done: true, task: "Code refactor" },
+    { done: false, task: "TO & FO" },
+    { done: false, task: "Fix docker for backend" },
+    { done: false, task: "Svelte warnings for unset properties" },
+    { done: false, task: "Tauri app" },
+    { done: false, task: "Play test!" }
+  ];
 </script>
 
 <svelte:head>
-	<title>EEN - TODO's</title>
+  <title>EEN - TODO's</title>
 </svelte:head>
 
 <Breadcrumb
-	aria-label="Menu"
-	class="p-4 bg-white md:flex md:p-6 dark:bg-gray-800 z-20 w-full rounded-none border-none"
-	solid
+  aria-label="Menu"
+  class="p-4 bg-white md:flex md:p-6 dark:bg-gray-800 z-20 w-full rounded-none border-none"
+  solid
 >
-	<BreadcrumbItem href="/" home>EEN</BreadcrumbItem>
-	<BreadcrumbItem href="/join">TODO's</BreadcrumbItem>
+  <BreadcrumbItem href="/" home>EEN</BreadcrumbItem>
+  <BreadcrumbItem href="/join">TODO's</BreadcrumbItem>
 </Breadcrumb>
 
 <div class="grid md:grid-cols-3 grid-cols-1 gap-3 m-3">
-	<div class="hidden md:block"><!-- Placeholder --></div>
-	<Card size="none">
-		<Heading tag="h2" class="text-center">TODO's</Heading>
-		<Listgroup items={todos} let:item class="my-3">
-			<Checkbox disabled checked={item.done}>{item.task}</Checkbox>
-		</Listgroup>
-		<Button href="/">Back</Button>
-	</Card>
-	<div class="hidden md:block"><!-- Placeholder --></div>
+  <div class="hidden md:block"><!-- Placeholder --></div>
+  <Card size="none">
+    <Heading tag="h2" class="text-center">TODO's</Heading>
+    <Listgroup items={todos} let:item class="my-3">
+      <Checkbox disabled checked={item.done}>{item.task}</Checkbox>
+    </Listgroup>
+    <Button href="/">Back</Button>
+  </Card>
+  <div class="hidden md:block"><!-- Placeholder --></div>
 </div>
