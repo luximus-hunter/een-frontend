48a3509ce7a84b13e2616f0d30822b3fc65c8dfd
luximus-hunter <thomaslipman10@gmail.com>
2023-03-15 16:13:02 +0100

    ..

--- src/routes/join/+page.server.ts
+++ src/routes/join/+page.server.ts
@@ -26,7 +26,7 @@ export const actions = {
 					method: 'post',
 					mode: 'no-cors',
 					headers: {
-						'Access-Control-Allow-Origin': '0.0.0.0:5000'
+						'Access-Control-Allow-Origin': '*'
 					}
 				}
 			);
