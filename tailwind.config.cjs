661ebf3c28ee8677922b291b26c045534fa61810
luximus-hunter <thomaslipman10@gmail.com>
2023-03-15 20:48:28 +0100

    Dark mode color to gray

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,21 @@
+const { neutral, indigo } = require("tailwindcss/colors");
 const config = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-	],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
+  ],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {
+      colors: {
+				blue: indigo,
+				gray: neutral
+      }
+    }
+  },
 
-	plugins: [require('flowbite/plugin')],
-	darkMode: 'media'
+  plugins: [require("flowbite/plugin")],
+  darkMode: "media"
 };
 
 module.exports = config;
